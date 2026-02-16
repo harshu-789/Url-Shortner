92175700164ccf9f8a33d0a297513d1519c0154a
Rishesha Harsh <risheshaharsh@gmail.com>
2026-02-17 02:09:06 +0530

    kjkl

--- Backend/app.js
+++ Backend/app.js
@@ -24,34 +24,16 @@ const allowedOrigins = [
 ];
 console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (
-        !origin ||
-        origin.includes("localhost") ||
-        origin.includes("vercel.app")
-      ) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
