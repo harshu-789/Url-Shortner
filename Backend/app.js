980838c04d289f2e2593a22cf0b9cbc0b80cee45
Rishesha Harsh <risheshaharsh@gmail.com>
2026-02-17 01:44:59 +0530

    solulu

--- Backend/app.js
+++ Backend/app.js
@@ -23,18 +23,23 @@ const allowedOrigins = [
 ];
 console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
