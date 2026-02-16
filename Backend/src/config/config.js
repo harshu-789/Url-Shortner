7b6f5b6794e0581e6b90576998d1adb9592e31f0
Rishesha Harsh <risheshaharsh@gmail.com>
2026-02-17 00:24:45 +0530

    Finally Solved

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -1,6 +1,14 @@
+// export const cookieOptions = {
+//     httpOnly: true,
+//     secure: process.env.NODE_ENV === "production",
+//     sameSite: "Lax",
+//     maxAge: 1000 * 60 * 60, // 5 minutes
+// }
+
+
 export const cookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Lax",
-    maxAge: 1000 * 60 * 60, // 5 minutes
-}
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  maxAge: 1000 * 60 * 60, // 1 hour
+};
