2e164d5f9f2858e89878cff102680beddc1125f5
Rishesha Harsh <risheshaharsh@gmail.com>
2026-04-08 20:10:17 +0530

    fix: update short URL response to use baseUrl and correct slug parameter in createCustomShortUrl

--- Backend/src/controllers/short.url.controller.js
+++ Backend/src/controllers/short.url.controller.js
@@ -13,7 +13,7 @@ export const createShortUrl = wrapAsync(async (req, res) => {
   } else {
     shortUrl = await createShortUrlWithoutUser(data.url);
   }
-  res.status(200).json({ shortUrl: process.env.APP_URL + shortUrl });
+  res.status(200).json({ shortUrl: baseUrl + shortUrl });
 });
 
 export const redirectShortUrl = wrapAsync(async (req, res) => {
@@ -25,6 +25,6 @@ export const redirectShortUrl = wrapAsync(async (req, res) => {
 
 export const createCustomShortUrl = wrapAsync(async (req, res) => {
   const { url, slug } = req.body;
-  const shortUrl = await createShortUrlWithoutUser(url, customUrl);
-  res.status(200).json({ shortUrl: process.env.APP_URL + shortUrl });
+  const shortUrl = await createShortUrlWithoutUser(url, slug);
+  res.status(200).json({ shortUrl: baseUrl + shortUrl });
 });
